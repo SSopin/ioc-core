b292faccd4ef45eb9f5958ba3817c0c58142f061
Sergii Sopin <Sergii_Sopin@epam.com>
2021-02-01 22:08:58 -0500

    private constructor added to ProcessorFactory class.

--- src/main/java/dev/sopin/ProcessorFactory.java
+++ src/main/java/dev/sopin/ProcessorFactory.java
@@ -1,6 +1,9 @@
 package dev.sopin;
 
 public class ProcessorFactory {
+
+    private ProcessorFactory() {}
+
     public static Processor createProcessor(AbstractProcessorFactory factory) {
         return factory.createProcessor();
     }
